148cd15a4b26c672dcea0ce7d3ab2c4d68d2d6bb
Ryan Shaw-Harrison <srazash@protonmail.com>
2022-12-02 12:46:15 +0000

    Day 2, Puzzle 2 complete!

---
+++ day02/puzzle2/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// create an empty 2d array (slice)
+	games := [][]string{}
+
+	// open the input file
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// scan the input file line by line into the array
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		ln := strings.Split(scanner.Text(), " ")
+		games = append(games, ln)
+	}
+
+	// initialise a score counter
+	score := 0
+
+	// iterate over the array, match and score the elements for each game
+	for idx := range games {
+		switch {
+		case games[idx][0] == "A":
+			switch {
+			case games[idx][1] == "X":
+				// lose against rock
+				score += 3 // 3 points for scissors
+			case games[idx][1] == "Y":
+				// draw against rock
+				score += 1 // 1 point for rock
+				score += 3 // 3 points for a draw
+			case games[idx][1] == "Z":
+				// win against rock
+				score += 2 // 2 points for paper
+				score += 6 // 6 points for winning
+			}
+		case games[idx][0] == "B":
+			switch {
+			case games[idx][1] == "X":
+				// lose against paper
+				score += 1 // 1 point for rock
+			case games[idx][1] == "Y":
+				// draw against paper
+				score += 2 // 2 points for paper
+				score += 3 // 3 points for a draw
+			case games[idx][1] == "Z":
+				// win against paper
+				score += 3 // 3 points for scissors
+				score += 6 // 6 points for winning
+			}
+		case games[idx][0] == "C":
+			switch {
+			case games[idx][1] == "X":
+				// lose against scissors
+				score += 2 // 2 points for paper
+			case games[idx][1] == "Y":
+				//draw against scissors
+				score += 3 // 3 points for scissors
+				score += 3 // 3 points for a draw
+			case games[idx][1] == "Z":
+				// win against scissors
+				score += 1 // 1 points for rock
+				score += 6 // 6 points for winning
+			}
+		}
+	}
+
+	fmt.Printf("Total score is: %d\n", score)
+
+}
